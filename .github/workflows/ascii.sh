2c3cbdefdd6ab79640bbab8bda64d21f53518906
prabhakar732 <prabhakar732@gmail.com>
2025-04-01 19:22:58 +0530

    Delete .github/workflows/ascii.sh

--- .github/workflows/ascii.sh
+++
@@ -1,9 +0,0 @@
-/usr/bin/bash
-
-chmod +x
-ls -lrth
-cowsay -f dragon " Run for cover, I a Dragon RAWR" >> dragon.txt
-grep -i "dragon" dragon.txt
-cat dragon.txt
-echo "hello Prabhakar"
-echo " am doing fine"
